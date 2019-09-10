fc9764a9437682100dfe39e736e61f5f158ae694
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-09 23:49:08 -0300

    build: Improve lunch error handling
    Change-Id: I78bd7f96d3cc47a9bdf8730dd8005610235b02e6

--- envsetup.sh
+++ envsetup.sh
@@ -795,11 +795,23 @@ function lunch()
 
     if [ "$1" ]; then
         answer=$1
+        if (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
+        then
+            echo
+            echo "Invalid lunch combo"
+            return 1
+        fi
     else
         print_lunch_menu
-        echo "Which would you like? [aosp_arm-trunk_staging-eng]"
-        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-trunk_staging-eng): "
+        echo -n "Which would you like? "
+        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-eng): "
         read answer
+        if ! (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
+        then
+            echo
+            echo "Invalid lunch combo"
+            return 1
+        fi
         used_lunch_menu=1
     fi
 
@@ -807,10 +819,16 @@ function lunch()
 
     if [ -z "$answer" ]
     then
+<<<<<<< HEAD
         selection=aosp_arm-trunk_staging-eng
+=======
+        echo
+        echo "Invalid lunch combo"
+        return 1
+>>>>>>> f8fddb6482 (build: Improve lunch error handling)
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
-        if [ $answer -le ${#choices[@]} ]
+        if [ $answer -ge 1 ] && [ $answer -le ${#choices[@]} ]
         then
             # array in zsh starts from 1 instead of 0.
             if [ -n "$ZSH_VERSION" ]
@@ -819,6 +837,10 @@ function lunch()
             else
                 selection=${choices[$(($answer-1))]}
             fi
+        else
+            echo
+            echo "Invalid lunch combo"
+            return 1
         fi
     else
         selection=$answer
