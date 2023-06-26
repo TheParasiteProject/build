5e7f98e354d93794cbd8c8ceafdaae7b5cea9b69
someone5678 <nemui3353@gmail.com>
2023-06-27 03:35:39 +0900

    tools: releasetools: Rebrand to this project
    * To prevent confusion
    
    Change-Id: I02c25c57b1d03076f3e892b9985fe6a9d418cab7

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -284,15 +284,10 @@ class EdifyGenerator(object):
     """Log a message to the screen (if the logs are visible)."""
     self.script.append('ui_print("%s");' % (message,))
 
-  def PrintPixelExperienceBanner(self, is_plus, android_version, build_id, build_date,
+  def PrintPixelExperienceBanner(self, android_version, build_id, build_date,
                                   security_patch, device):
     self.Print("----------------------------------------------")
-    if is_plus:
-      self.Print("        PixelExperience (Plus edition)")
-      self.Print("                by jhenrique09")
-    else:
-      self.Print("              PixelExperience")
-      self.Print("              by jhenrique09")
+    self.Print("              TheParasiteProject")
     self.Print("----------------------------------------------")
     self.Print(" Android version: %s"%(android_version))
     self.Print(" Build id: %s"%(build_id))
