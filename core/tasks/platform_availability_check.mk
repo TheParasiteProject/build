d3042f2b9e19e65cbfeded0703abaed1b34b7eb4
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-07 13:01:28 +0900

    build: Allow installing APEX internal module to system
    * When we add `//apex_available:platform`
    * Follows QCom's change[1] and extends to case when ALLOW_MISSING_DEPENDENCIES defined
    
    Ref:
    [1] https://git.codelinaro.org/clo/la/platform/build_repo/-/commit/0d525ac79c7e6c6c86ceba158deb847787478f14
    
    Change-Id: Ie9f3a5f03211da4c300cff5461ddaf138e32ff8a

--- core/tasks/platform_availability_check.mk
+++ core/tasks/platform_availability_check.mk
@@ -34,11 +34,6 @@ ifndef ALLOW_MISSING_DEPENDENCIES
     $(m):$(word 1,$(ALL_MODULES.$(m).PATH))\
   )
 
-  $(call maybe-print-list-and-error,$(_violators_with_path),\
-Following modules are requested to be installed. But are not available \
-for platform because they do not have "//apex_available:platform" or \
-they depend on other modules that are not available for platform)
-
 else
 
 # Don't error out immediately when ALLOW_MISSING_DEPENDENCIES is set.
@@ -53,9 +48,6 @@ it depends on other modules that are not available for platform.)
 	exit 1
   endef
 
-  $(foreach m,$(_modules_not_available_for_platform),\
-    $(foreach i,$(filter-out $(HOST_OUT)/%,$(ALL_MODULES.$(m).INSTALLED)),\
-      $(eval $(call not_available_for_platform_rule,$(i),$(m)))))
 endif
 
 endif
